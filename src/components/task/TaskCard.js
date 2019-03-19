49e3307a5007f79b0721d46378259580cc23c416
newforce <jppwellman@gmail.com>
2019-03-19 10:44:01 -0400

    updates to TaskList, TaskCard, and AppViews

--- src/components/task/TaskCard.js
+++ src/components/task/TaskCard.js
@@ -5,20 +5,20 @@ import './Task.css';
 export default class TaskCard extends Component {
     render() {
         return (
-            <div class="row">
+            <div className="row">
             <div key={this.props.task.id} className="col-lg-5 mb-lg-0 mb-4">
                 <div className="card">
                     <div className="card-body">
-                    <div class="form-header blue accent-1">
-                        <h3 className="mt-2"><i class="material-icons md-48">toc</i> Task Name:</h3>
+                    <div className="form-header blue accent-1">
+                        <h3 className="mt-2"><i className="material-icons md-48">toc</i> Task Name:</h3>
                         </div>
                         <h4 className="">{this.props.task.name}</h4>
                         </div>
                         <div>
-                        <button className="btn btn-info"><Link className="text-white" to={`/tasks/${this.props.task.id}`}>Task Details</Link></button>
+                        <button className="btn btn-info"><Link className="text-white" to={`/tasks/${this.props.task.id}`}><i className="material-icons md-18">details</i>Task Details</Link></button>
                         </div>
                         <div>
-                        <button className="btn btn-danger"><a href="/tasks/" onClick={() => this.props.deleteTask(this.props.task.id)} className="text-white" alt="Remove Task">Delete  Task</a></button>
+                        <button className="btn btn-danger"><a href="/tasks/" onClick={() => this.props.deleteTask(this.props.task.id)} className="text-white" alt="Remove Task"><i className="material-icons md-18">delete</i>Delete  Task</a></button>
                         </div>
                 </div>
             </div>
